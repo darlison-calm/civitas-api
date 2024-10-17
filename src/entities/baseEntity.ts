a35711ff23825ede59ce193a85639b51b5d6304a
agleicesousa <agleicesousa98@gmail.com>
2024-10-17 00:13:38 -0300

    fix/feat: add documentação TSDoc e movendo email para membros gerais

--- src/entities/baseEntity.ts
+++ src/entities/baseEntity.ts
@@ -5,42 +5,78 @@ import {
   BeforeUpdate
 } from 'typeorm';
 
+/**
+ * Enumeração para definir os diferentes tipos de contas de usuários.
+ */
 export enum TipoConta {
+  /**
+   * Conta de administrador.
+   */
   ADMIN = 'admin',
+
+  /**
+   * Conta de professor.
+   */
   PROFESSOR = 'professor',
+
+  /**
+   * Conta de aluno.
+   */
   ALUNO = 'aluno',
+
+  /**
+   * Conta de responsável.
+   */
   RESPONSAVEL = 'responsavel'
 }
 
+/**
+ * Entidade base abstrata que contém campos comuns a todas as entidades.
+ * Inclui campos de auditoria como `dataCriacao` e `dataAtualizacao`.
+ */
 export abstract class BaseEntity {
+  /**
+   * Chave primária gerada automaticamente para cada entidade.
+   * @type {number}
+   */
   @PrimaryGeneratedColumn()
   id: number;
 
+  /**
+   * Data de criação do registro.
+   * Definida automaticamente ao inserir um novo registro.
+   * @type {Date}
+   */
   @Column()
   dataCriacao: Date;
 
+  /**
+   * Data de atualização do registro.
+   * Definida automaticamente ao atualizar um registro.
+   * @type {Date}
+   */
   @Column()
   dataAtualizacao: Date;
 
-  @BeforeInsert()
   /**
-   * Define a data de criacao do registro.
-   * Chamada automaticamente antes de inserir um registro no banco de dados.
+   * Define a data de criação do registro.
+   * Este método é chamado automaticamente antes de um registro ser inserido no banco de dados.
    * @internal
    * @returns {void}
    */
-  setDataCriacao() {
+  @BeforeInsert()
+  setDataCriacao(): void {
     this.dataCriacao = new Date();
   }
 
-  @BeforeUpdate()
   /**
-   * Define a data de atualizacao do registro.
-   * Chamada automaticamente antes de atualizar um registro no banco de dados.
+   * Define a data de atualização do registro.
+   * Este método é chamado automaticamente antes de um registro ser atualizado no banco de dados.
    * @internal
    * @returns {void}
    */
-  setDataAtualizacao() {
+  @BeforeUpdate()
+  setDataAtualizacao(): void {
     this.dataAtualizacao = new Date();
   }
 }
--- src/entities/membrosEntities.ts
+++ src/entities/membrosEntities.ts
@@ -1,24 +1,60 @@
 import { Entity, Column } from 'typeorm';
-
 import { BaseEntity, TipoConta } from './baseEntity';
 
 @Entity()
 export class Membros extends BaseEntity {
+  /**
+   * Número de matrícula único associado ao membro.
+   * @type {string}
+   * @unique
+   */
   @Column({ unique: true })
   numeroMatricula: string;
 
+  /**
+   * Endereço de e-mail único do membro.
+   * @type {string}
+   * @unique
+   */
+  @Column({ unique: true })
+  email: string;
+
+  /**
+   * Nome completo do membro.
+   * @type {string}
+   */
   @Column()
   nomeCompleto: string;
 
+  /**
+   * Data de nascimento do membro.
+   * @type {Date}
+   */
   @Column()
   dataNascimento: Date;
 
+  /**
+   * Número do RG (Registro Geral) do membro.
+   * O RG deve ser único no banco de dados.
+   * @type {string}
+   * @unique
+   */
   @Column({ unique: true })
   rg: string;
 
+  /**
+   * Número do CPF (Cadastro de Pessoa Física) do membro.
+   * O CPF deve ser único no banco de dados.
+   * @type {string}
+   * @unique
+   */
   @Column({ unique: true })
   cpf: string;
 
+  /**
+   * Tipo de conta do membro, que pode ser um dos valores definidos no enum `TipoConta`.
+   * @type {TipoConta}
+   */
   @Column({ type: 'enum', enum: TipoConta })
   tipoConta: TipoConta;
 }
